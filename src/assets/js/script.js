eaff0146a51a547b471897c4df1cdcfc3ba99dd8
IAN63k <julian63k@gmail.com>
2022-12-18 20:50:29 -0500

    reemplazo de animaciónes

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -12,6 +12,19 @@ const navbar = document.querySelector('.navbar'),
 
 let counter = 2;
 
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) =>{
+        console.log(entry);
+        if (entry.isIntersecting){
+            entry.target.classList.add('show');
+        }else{
+            entry.target.classList.remove('show')
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((element) => observer.observe(element));
 /** --------------------------------
  -- Transparencia del navBar  --
 -------------------------------- */
